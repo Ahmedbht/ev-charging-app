01040a1097a085759175f53a9143a4b3b93aa82e
Ahmed <email@gmail.com>
2026-06-04 19:00:15 +0100

    backend complete - Fastapi EV

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ app =FastAPI()
 #contact with react back to front
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
